a4a16190113ead0330b0c7dc4e9ecabdc982a7e9
sahiler <srangwala1998@gmail.com>
2024-01-25 21:39:19 -0600

    updating name

--- workpool-switch-test.py
+++ workpool-switch-test.py
@@ -5,8 +5,8 @@ from subflow_example import pass_data_between_subflows
 if __name__ == "__main__":
     pass_data_between_subflows.from_source(
         source="https://github.com/Sahiler/demo-examples.git",
-        entrypoint="subflow-example.py:pass_data_between_subflows",
+        entrypoint="subflow_example.py:pass_data_between_subflows",
     ).deploy(
         name="test-push-pool-flow",
-        work_pool_name="easy-push-pool",
+        work_pool_name="easy-push-pool"
     )
